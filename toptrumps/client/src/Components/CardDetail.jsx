2e4475bd3e460e237d462eefaa10538f0ed36fc4
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-09-23 14:30:32 +0100

    added card details, renders on client

--- toptrumps/client/src/Components/CardDetail.jsx
+++ toptrumps/client/src/Components/CardDetail.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const CardDetail = () => {
-  return (
-    <div>CardDetail</div>
+const CardDetail = ({ card }) => {
+
+
+  return(
+  <>
+  <div>Name: {card.Individual}</div>
+  <div>Most Loveable: {card["Most Lovable"]}</div>
+  <div>Smartest: {card.Smartest}</div>
+  <div>Fattest: {card.Fattest}</div>
+  <div>Biggest Nerd: {card["Biggest Nerd"]}</div>
+  <div>Greatest Anarchist: {card["Greatest Anarchist"]}</div>
+  <div>Walk of Fame Rating: {card["Walk of Fame Rating"]}</div>
+  <img src={card.img_url} alt="" />
+</>
   )
-}
+};
 
-export default CardDetail
+export default CardDetail;
--- toptrumps/client/src/Components/CardGrid.jsx
+++ toptrumps/client/src/Components/CardGrid.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import CardDetail from "./CardDetail";
 
 const CardGrid = ({ cards }) => {
+  // if (!cards) return <h2>Loading...</h2>;
 
-
-  if (!cards) return <h2>Loading...</h2>
-    const CardList = cards.map((card)=>{
-      return(
-        <>
-       
-        <CardDetail card = {card} key = {card._id}/> 
+  const CardList = cards.map((card) => {
+    return (
+      <>
+        <ul>
+        <CardDetail card={card} key={card._id} />
         {/* delete needs to be drilled here later */}
+        </ul>
       </>
-      )
-    })
-
-
+    );
+  });
 
   return (
     <>
       <div>CardGrid</div>
-      <ul>{CardList}</ul>
+      {CardList}
     </>
   );
 };
