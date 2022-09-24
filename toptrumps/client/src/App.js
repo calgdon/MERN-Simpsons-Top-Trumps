d903fc15d035f405ac7640fd74e3c1abce1b9cac
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-09-24 15:43:18 +0100

    added selected card and duel

--- toptrumps/client/src/App.js
+++ toptrumps/client/src/App.js
@@ -52,39 +52,69 @@ function App() {
   // Shuffle main deck
 
   const shuffleDeck = () => {
-  let copyOfCards = cards.map((cardInDeck) => cardInDeck)
-  const shuffledDeck = copyOfCards.sort(() => Math.random() - 0.5)
-  setCards(shuffledDeck)
+    let copyOfCards = cards.map((cardInDeck) => cardInDeck)
+    const shuffledDeck = copyOfCards.sort(() => Math.random() - 0.5)
+    setCards(shuffledDeck)
   }
 
   // Deal deck of cards
 
   const dealDeck = () => {
     let playerAssignment = 0
-    let dealerCards = [[],[]]
-    cards.forEach((card)=>{
-      let playerNumber = (playerAssignment%2)
+    let dealerCards = [[], []]
+    cards.forEach((card) => {
+      let playerNumber = playerAssignment % 2
       dealerCards[playerNumber].push(card)
       playerAssignment += 1
     })
-    addCardToDeck(dealerCards[0],1)
-    addCardToDeck(dealerCards[1],2)
+    addCardToDeck(dealerCards[0], 1)
+    addCardToDeck(dealerCards[1], 2)
   }
-
-
-
 
   // GAME MECHANICS
 
-  
+  let [controllingPlayer, setControllingPlayer] = useState(1)
+  let [player1Card, setPlayer1Card] = useState()
+  let [player2Card, setPlayer2Card] = useState()
+
+  // assign selected cards
+
+  const selectCards = () => {
+    setPlayer1Card(player1Deck[0])
+    deleteCardFromDeck(player1Card, 1) 
+    setPlayer2Card(player2Deck[0])
+    deleteCardFromDeck(player2Card, 2) 
+  }
 
 
+  // Comparison function
 
+  const decideWinner = (attribute) => {
+    if (controllingPlayer == 1) {
+      if (player1Card[attribute] > player2Card[attribute]) {
+        addCardToDeck(player1Card, 1), addCardToDeck(player2Card, 1)
+      } else {
+        addCardToDeck(player1Card, 2),
+          addCardToDeck(player2Card, 2),
+          setControllingPlayer(2)
+      }
+    }
+    if (controllingPlayer == 2) {
+      if (player2Card[attribute] > player1Card[attribute]) {
+        addCardToDeck(player2Card, 2), addCardToDeck(player1Card, 2)
+      } else {
+        addCardToDeck(player2Card, 1),
+          addCardToDeck(player1Card, 1),
+          setControllingPlayer(1)
+      }
+    }
+  }
 
   // Testing of functions
 
   const handleClickAdd = () => {
-    addCardToDeck(cards[5], 1)
+    addCardToDeck(cards[1], 1)
+    addCardToDeck(cards[3], 2)
   }
 
   const handleClickDelete = () => {
@@ -99,7 +129,13 @@ function App() {
     dealDeck()
   }
 
+  const handleClickSelect = () => {
+    selectCards()
+  }
 
+  const handleClickDuel = () => {
+    decideWinner("Fattest")
+  }
 
   // addCardToDeck(cards[0], 1)
 
@@ -110,6 +146,8 @@ function App() {
       <button onClick={handleClickDelete}>TESTING DELETE</button>
       <button onClick={handleClickShuffle}>TESTING SHUFFLE</button>
       <button onClick={handleClickDeal}>TESTING DEAL</button>
+      <button onClick={handleClickSelect}>TESTING SELECT</button>
+      <button onClick={handleClickDuel}>TESTING DUEL</button>
 
       <ViewCardsPage cards={cards} />
     </>
