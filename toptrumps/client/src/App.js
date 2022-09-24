15114033d79f298ee521139950187f06395b27c8
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-09-24 14:02:29 +0100

    added shuffle feature

--- toptrumps/client/src/App.js
+++ toptrumps/client/src/App.js
@@ -30,6 +30,8 @@ function App() {
     setDeck(copyOfPlayerDeck)
   }
 
+  // Delete a single card from a deck
+
   const deleteCardFromDeck = (card, playerNumber) => {
     let playerDeck = undefined
     let setDeck = undefined
@@ -40,18 +42,49 @@ function App() {
       playerDeck = player2Deck
       setDeck = setPlayer2Deck
     }
-    const copyOfPlayerDeck = playerDeck.map((cardInDeck) => cardInDeck) 
+    const copyOfPlayerDeck = playerDeck.map((cardInDeck) => cardInDeck)
     const cardIndex = copyOfPlayerDeck.indexOf(card)
-    copyOfPlayerDeck.splice(cardIndex,1)
+    copyOfPlayerDeck.splice(cardIndex, 1)
     setDeck(copyOfPlayerDeck)
   }
+
+  // shuffle main deck
+
+  // const shuffleDeck = () => {
+  //   let copyOfCards = cards.map((cardInDeck) => cardInDeck)
+  //   let currentIndex = copyOfCards.length,
+  //     randomIndex
+  //   while (currentIndex != 0) {
+  //     randomIndex = Math.floor(Math.random() * currentIndex)
+  //     currentIndex--
+  //     ;[copyOfCards[currentIndex], copyOfCards[randomIndex]] = [
+  //       copyOfCards[randomIndex],
+  //       copyOfCards[currentIndex],
+  //     ]
+  //   }
+  //   setCards(copyOfCards)
+  // }
+
+
+  // Shuffle main deck
+
+  const shuffleDeck = () => {
+  let copyOfCards = cards.map((cardInDeck) => cardInDeck)
+  const shuffledDeck = copyOfCards.sort(() => Math.random() - 0.5)
+  setCards(shuffledDeck)
+  }
+ 
 
   const handleClickAdd = () => {
     addCardToDeck(cards[0], 1)
   }
 
   const handleClickDelete = () => {
-    deleteCardFromDeck(player1Deck[0],1)
+    deleteCardFromDeck(player1Deck[0], 1)
+  }
+
+  const handleClickShuffle = () => {
+    shuffleDeck()
   }
 
   // addCardToDeck(cards[0], 1)
@@ -61,6 +94,7 @@ function App() {
       <h1>Welcome to TopTrumps</h1>
       <button onClick={handleClickAdd}>TESTING ADD</button>
       <button onClick={handleClickDelete}>TESTING DELETE</button>
+      <button onClick={handleClickShuffle}>TESTING SHUFFLE</button>
 
       <ViewCardsPage cards={cards} />
     </>
