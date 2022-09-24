e3de064e36cca21894b08a074f002ec227c0016b
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-09-24 12:28:05 +0100

    addCardToDeck function added

--- toptrumps/client/src/App.js
+++ toptrumps/client/src/App.js
@@ -1,22 +1,50 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import ViewCardsPage from "./Containers/ViewCardsPage";
-import TopTrumpsService from "./services/TopTrumpsService"
-
+import React from 'react'
+import { useEffect, useState } from 'react'
+import './App.css'
+import ViewCardsPage from './containers/ViewCardsPage'
+import TopTrumpsService from './services/TopTrumpsService'
 
 function App() {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState([])
+  const [player1Deck, setPlayer1Deck] = useState([])
+  const [player2Deck, setPlayer2Deck] = useState([])
 
   useEffect(() => {
-    TopTrumpsService.getTopTrumps().then((cards) => setCards(cards));
-  },[]);
+    TopTrumpsService.getTopTrumps().then((cards) => setCards(cards))
+  }, [])
+
+  // add a single card to a players deck. Have to pass in actual player Number
+
+  const addCardToDeck = (card, playerNumber) => {
+    let playerDeck = undefined
+    let setDeck = undefined
+    if (playerNumber === 1) {
+      playerDeck = player1Deck
+      setDeck = setPlayer1Deck
+    } else {
+      playerDeck = player2Deck
+      setDeck = setPlayer2Deck
+    }
+    const copyOfPlayerDeck = playerDeck.map((card) => card)
+    copyOfPlayerDeck.push(card)
+    setDeck(copyOfPlayerDeck)
+  }
+
+
+  const handleClick = () => {
+    addCardToDeck(cards[0], 1)
+  }
+
+  // addCardToDeck(cards[0], 1)
 
   return (
     <>
       <h1>Welcome to TopTrumps</h1>
-      <ViewCardsPage cards={cards}/>
+      <button onClick={handleClick}>TESTING</button>
+
+      <ViewCardsPage cards={cards} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
