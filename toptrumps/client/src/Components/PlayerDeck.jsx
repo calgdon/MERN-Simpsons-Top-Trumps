67a7acc3be0ce927e7d17979f9bd5694517a8d9a
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-09-27 16:32:17 +0100

    player deck generating (no css)

--- toptrumps/client/src/Components/PlayerDeck.jsx
+++ toptrumps/client/src/Components/PlayerDeck.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import toptrumpback from "../images/toptrumpback.jpeg";
 
-const PlayerDeck = () => {
-  return (
-    <img className="trumpCardBack" src={toptrumpback} />
-  )
-}
+{/* <li>
+  <img className="trumpCardBack" src={toptrumpback} />
+</li>; */}
+const PlayerDeck = ({ deck }) => {
+  const deckRender = deck.map(() => {
+    return (
+      <li>
+        <img className="trumpCardBack" src={toptrumpback} />
+      </li>
+    );
+  });
+  console.log(deckRender)
 
-export default PlayerDeck
+  return <ul>{deckRender}</ul>;
+};
+
+export default PlayerDeck;
