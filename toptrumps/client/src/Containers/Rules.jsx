73446ea357dcd17a03e16e6406aca0176450028c
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-09-27 10:05:15 +0100

    rules css relinked

--- toptrumps/client/src/Containers/Rules.jsx
+++ toptrumps/client/src/Containers/Rules.jsx
@@ -6,6 +6,8 @@ const Rules = () => {
     return (
       <>
     <h2>Rules</h2>
+    <div id="rules-div">
+    <ul id='myUL'>
     <li>Win game by being the first player to win all the cards.</li>
     <li>Two or more players can play Top Trumps. Shuffle the cards and give each player an equal number of cards.</li>
     <li>Players must keep all their cards in a pile, without seeing other players' cards.  Only look at the top card.</li>
@@ -13,6 +15,8 @@ const Rules = () => {
     <li>Other players then look at their top card and compare the stat from the declared category.</li>
     <li>The player that has the highest stat wins the round.  They take the losing card or cards and put them at the bottom of their pile.</li>
     <li>The winner is the player with all the cards at the end of the game.</li>
+    </ul>
+    </div>
       </>
     
     )
