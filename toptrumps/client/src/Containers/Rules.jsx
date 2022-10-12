587a17af2d1cb4db4a41cc7402d539c76181d4b4
Callum Donaldson <108418393+calgdon@users.noreply.github.com>
2022-10-12 09:26:25 +0100

    Update Rules.jsx

--- toptrumps/client/src/Containers/Rules.jsx
+++ toptrumps/client/src/Containers/Rules.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import NavBar from '../components/Navbar'
+import NavBar from '../Components/Navbar'
 import '../css/Master.css'
 
 const Rules = () => {
